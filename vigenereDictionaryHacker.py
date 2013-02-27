098f6eee943ca834c57ac64b22ebad305887b2c1
Al Sweigart <asweigart@gmail.com>
2013-02-26 19:26:43 -0800

    Corrected the URL.

--- vigenereDictionaryHacker.py
+++ vigenereDictionaryHacker.py
@@ -1,5 +1,5 @@
 # Vigenere Cipher Dictionary Hacker
-# http://inventwithpython.com/codebreaker (BSD Licensed)
+# http://inventwithpython.com/hacking (BSD Licensed)
 
 import detectEnglish, vigenereCipher, pyperclip
 
