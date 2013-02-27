5170026bef81c71367efb80bdc8df66cf46e4e51
Al Sweigart <asweigart@gmail.com>
2013-02-26 20:08:26 -0800

    Adding caesarCipher2.py

---
+++ caesarCipher2.py
@@ -0,0 +1,52 @@
+# Caesar Cipher
+# http://inventwithpython.com/hacking (BSD Licensed)
+
+import pyperclip
+
+# the string to be encrypted/decrypted
+message = 'This is my secret message.'
+
+# the encryption/decryption key
+key = 13
+
+# tells the program to encrypt of decrypt
+mode = 'encrypt' # set to 'encrypt' or 'decrypt'
+
+# every possible symbol that can be encrypted
+LETTERS = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
+
+# stores the encrypted/decrypted form of the message
+translated = ''
+
+# capitalize the string in message
+#message = message.upper()
+
+# run the encryption/decryption code on each symbol in the message string
+for symbol in message:
+    if symbol in LETTERS:
+        # get the encrypted (or decrypted) number for this symbol
+        num = LETTERS.find(symbol) # get the number of the symbol
+        if mode == 'encrypt':
+            num = num + key
+        elif mode == 'decrypt':
+            num = num - key
+
+        # handle the wrap around if num is larger than the length of
+        # LETTERS or less than 0
+        if num >= len(LETTERS):
+            num = num - len(LETTERS)
+        elif num < 0:
+            num = num + len(LETTERS)
+
+        # add encrypted/decrypted number's symbol at the end of translated
+        translated = translated + LETTERS[num]
+
+    else:
+        # just add the symbol without encrypting/decrypting
+        translated = translated + symbol
+
+# print the encrypted/decrypted string to the screen
+print(translated)
+
+# copy the encrypted/decrypted string to the clipboard
+pyperclip.copy(translated)
